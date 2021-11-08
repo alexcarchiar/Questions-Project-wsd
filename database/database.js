6a9f1326502a9fafba2e46c9f217b13bf81e8976
Chiarelli Alessandro <alexcarchiar@gmail.com>
2021-11-08 22:05:45 +0200

    Initial commit

---
+++ database/database.js
@@ -0,0 +1,37 @@
+import { Pool } from "../deps.js";
+
+const CONCURRENT_CONNECTIONS = 2;
+//const connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
+const connectionPool = new Pool({
+  hostname: "hattie.db.elephantsql.com",
+  database: "wcphofqf",
+  user: "wcphofqf",
+  password: "sX40biZQp4LdJ3ZfMl6vYe4xH5h0wI-Y",
+  port: 5432,
+}, CONCURRENT_CONNECTIONS);
+
+const executeQuery = async (query, ...args) => {
+  const response = {};
+  let client;
+
+  try {
+    client = await connectionPool.connect();
+    const result = await client.queryObject(query, ...args);
+    if (result.rows) {
+      response.rows = result.rows;
+    }
+  } catch (e) {
+    console.log(e);
+    response.error = e;
+  } finally {
+    try {
+      await client.release();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return response;
+};
+
+export { executeQuery };
---
+++ routes/controllers/mainController.js
@@ -0,0 +1,9 @@
+const showMain = ({ render }) => {
+  render("main.eta");
+};
+
+const createAccount;
+
+const login;
+
+export { showMain, createAccount, login };
