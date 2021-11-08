cca84cacb03e302ba787ae1c92f9fd86715d1967
Chiarelli Alessandro <alexcarchiar@gmail.com>
2021-11-08 22:09:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import renderMiddleware from "./middlewares/renderMiddleware.js";
 import { serveStaticMiddleware } from "./middlewares/serveStaticMiddleware.js";
 import { router } from "./routes/routes.js";
+import { authMiddleware } from "./middlewares/authMiddleware.js";
 
 const app = new Application();
 
 app.use(errorMiddleware);
 app.use(serveStaticMiddleware);
 app.use(renderMiddleware);
+app.use(authMiddleware)
 app.use(router.routes());
 
 export { app };
