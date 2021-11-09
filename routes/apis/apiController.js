ce7ce5ca9e998a17cd929483332f81da70acd3ba
Chiarelli Alessandro <alexcarchiar@gmail.com>
2021-11-10 00:51:32 +0200

    API completed

--- routes/apis/apiController.js
+++ routes/apis/apiController.js
@@ -1,9 +1,43 @@
-const getRandomQuestion = async () => {
+import * as questionsService from "../../services/questionsService.js"
+import * as optionsService from "../../services/optionsService.js"
 
+const getRandomQuestion = async ({response}) => {
+    let question = await questionsService.getRandomQuestion()
+
+    question.answerOptions = await optionsService.getOptions(question.id)
+
+    let data = {
+        questionId: question.id,
+        questionTitle: question.title,
+        questionText: question.question_text,
+        answerOptions: [],
+    }
+    question.answerOptions.forEach(el => {
+        let d = {
+            optionId: el.id,
+            optionText: el.option_text,
+        }
+        data.answerOptions.push(d)
+    })
+    response.body = data
 }
 
-const answerQuestion = async () => {
+const answerQuestion = async ({ request, response}) => {
+    const body = request.body({ type: "json" });
+    const document = await body.value;
+    if(document.questionId && document.optionId){
+        const option = (await optionsService.getOptionById(document.optionId))[0]
 
+        if(option.is_correct){
+            response.body = {
+                correct: true,
+            }
+        } else {
+            response.body = {
+                correct: false,
+            }
+        }
+    }
 }
 
 export { getRandomQuestion, answerQuestion}
